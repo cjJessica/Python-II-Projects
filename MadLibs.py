471cad2af21baa733f8d19ee63fa750d799f4ae6
Charli Pape <111802882+cjJessica@users.noreply.github.com>
2025-09-12 12:43:36 -0700

    Create MadLibs.py

---
+++ MadLibs.py
@@ -0,0 +1,10 @@
+adjective = input("Enter an adjective: ")
+animal = input("Enter an animal: ")
+food = input("Enter a food: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+verb1 = input("Enter another verb: ")
+
+story = "Once upon a time a " + animal + " was " + verb + "ing around. It then came across a " + place + ". The animalda was so " + adjective + " that it grabbed it's " + food + " and started to " + verb1
+
+print(story)
